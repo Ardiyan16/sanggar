d7467c2b103a39b5a6191681f1d7d51a7bccd31b
Ardiyan16 <47960612+Ardiyan16@users.noreply.github.com>
2021-06-14 18:35:48 +0700

    Update v_penyewaan.php

--- application/views/backoffice/page/v_penyewaan.php
+++ application/views/backoffice/page/v_penyewaan.php
@@ -152,21 +152,19 @@
                             $i = 1;
                             foreach ($sewa_new as $d) { ?>
                                 <tr>
-                                    <form method="post" action="<?= base_url('backoffice/penyewaan/kirim_notif') ?>">
-                                        <td><?= $i++ ?></td>
-                                        <td><?= $d->nama ?><input type="hidden" value="<?= $d->id_user ?>" name="id_user"></td>
-                                        <td><?= $d->tanggal_sewa ?></td>
-                                        <td><?= $d->tanggal_kembali ?></td>
-                                        <td><?= $d->total_kostum ?></td>
-                                        <td><?= $d->total_pembayaran ?></td>
-                                        <td><?= $d->metode_pembayaran ?></td>
-                                        <td><img class="rounded" width="64px" src="<?= base_url('assets/images/bukti_tf/' . $d->foto_tf) ?>"></td>
-                                        <td><?= $d->nama_lengkap ?></td>
-                                        <td>
-                                            <a class="btn btn-success btn-sm " href="<?= base_url('backoffice/penyewaan/arsip/' . $d->id_penyewaan) ?>"><i class="fas fa-archive"></i> Arsipkan</a>
-                                            <button class="btn btn-info btn-sm " type="submit"><i class="fas fa-bell"></i> Kirim Notifikasi</button>
-                                        </td>
-                                    </form>
+                                    <td><?= $i++ ?></td>
+                                    <td><?= $d->nama ?><input type="hidden" value="<?= $d->id_user ?>" name="id_user"></td>
+                                    <td><?= $d->tanggal_sewa ?></td>
+                                    <td><?= $d->tanggal_kembali ?></td>
+                                    <td><?= $d->total_kostum ?></td>
+                                    <td><?= $d->total_pembayaran ?></td>
+                                    <td><?= $d->metode_pembayaran ?></td>
+                                    <td><img class="rounded" width="64px" src="<?= base_url('assets/images/bukti_tf/' . $d->foto_tf) ?>"></td>
+                                    <td><?= $d->nama_lengkap ?></td>
+                                    <td>
+                                        <a class="btn btn-success btn-sm " href="<?= base_url('backoffice/penyewaan/arsip/' . $d->id_penyewaan) ?>"><i class="fas fa-archive"></i> Arsipkan</a>
+                                        <a class="btn btn-info btn-sm " href="<?= base_url('backoffice/penyewaan/v_kirim_notif/' . $d->id_penyewaan) ?>"><i class="fas fa-bell"></i> Kirim Notifikasi</a>
+                                    </td>
                                 </tr>
                             <?php } ?>
                         </tbody>
