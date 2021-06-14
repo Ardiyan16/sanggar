306686908891c9e4b15dcfa92b17af587eb47e05
Ardiyan16 <47960612+Ardiyan16@users.noreply.github.com>
2021-06-14 13:26:30 +0700

    d

--- application/views/frontoffice/style/v_navbar_user2.php
+++ application/views/frontoffice/style/v_navbar_user2.php
@@ -55,7 +55,7 @@
                 <form>
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Foto Transfer</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Pesan Notifikasi</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
